15ee11b9932aaea79d0ac673371100a9b545ac76
Charles-Chrismann <charles.chrismann@free.fr>
2023-04-06 22:13:39 +0200

    :sparkles: ADD slider

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -11,34 +11,49 @@ use Symfony\UX\Chartjs\Model\Chart;
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_app')]
-    public function index(ChartBuilderInterface $chartBuilder): Response
+    public function index(): Response
     {
-        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
 
-        $chart->setData([
-            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            'datasets' => [
-                [
-                    'label' => 'My First dataset',
-                    'backgroundColor' => 'rgb(255, 99, 132)',
-                    'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => [0, 10, 5, 2, 20, 30, 45],
-                ],
-            ],
-        ]);
-
-        $chart->setOptions([
-            'scales' => [
-                'y' => [
-                    'suggestedMin' => 0,
-                    'suggestedMax' => 100,
-                ],
-            ],
-        ]);
 
         return $this->render('app/index.html.twig', [
-            'fullName' => 'Wolrd',
-            'chart' => $chart
+            'reviews' => [
+                [
+                    'content' => 'lorem ipsum',
+                    'firstName' => 'Marie',
+                    'age' => 41
+                ],
+                [
+                    'content' => 'lorem ipsum',
+                    'firstName' => 'Marie',
+                    'age' => 41
+                ],
+                [
+                    'content' => 'lorem ipsum',
+                    'firstName' => 'Marie',
+                    'age' => 41
+                ],
+                [
+                    'content' => 'lorem ipsum',
+                    'firstName' => 'Marie',
+                    'age' => 41
+                ],
+                [
+                    'content' => 'lorem ipsum',
+                    'firstName' => 'Marie',
+                    'age' => 41
+                ],
+                [
+                    'content' => 'lorem ipsum',
+                    'firstName' => 'Marie',
+                    'age' => 41
+                ],
+                [
+                    'content' => 'lorem ipsum',
+                    'firstName' => 'Marie',
+                    'age' => 41
+                ],
+
+            ]
         ]);
     }
 }

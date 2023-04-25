a3828b9fe21c98d1f6621f2f0e7553298fa2ba9b
deez-blip <gaspard.castagnet92@gmail.com>
2023-04-25 14:43:41 +0200

    survey save with submiter page/controller

--- src/Controller/SurveyAnswerSaveController.php
+++ src/Controller/SurveyAnswerSaveController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Answer;
 use App\Entity\Question;
+use App\Entity\Submitter;
 use App\Repository\QuestionRepository;
+use App\Repository\SurveyRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,15 +17,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class SurveyAnswerSaveController extends AbstractController
 {
     #[Route('/survey/answer/save', name: 'app_survey_answer_save')]
-    public function index(Request $request, EntityManagerInterface $manager, QuestionRepository $questionRepository): Response
+    public function index(Request $request, EntityManagerInterface $manager, QuestionRepository $questionRepository, SurveyRepository $surveyRepository): Response
     {
         //dd($request->request->all());
         $requestData = $request->request->all();
         $keysArray = array_keys($requestData);
+
+        $survey_id = $requestData["survey_ID"];
+        $survey = $surveyRepository->find($survey_id);
+        //dd($survey_id);
+
+        $submitter = new Submitter();
+        $submitter->setSurvey($survey);
+
+        $manager->persist($submitter);
+        $manager->flush();
+
         $index = 1;
         //dd($keysArray[0]);
         //dd($question);
-        //zz
 
         foreach($requestData as $data) {
             if($data != "Envoyer") {
@@ -33,6 +45,7 @@ class SurveyAnswerSaveController extends AbstractController
                 $answer = new Answer();
                 $answer->setQuestion($question);
                 $answer->setContent($data);
+                $answer->setSubmitter($submitter);
 
                 $manager->persist($answer);
 
